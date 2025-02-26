ef7d298eb1be747877639b31f969b9c5cd3c9546
Connor Kuhn <connorisoutside@gmail.com>
2025-02-26 16:19:48 -0700

    Update deps to the 2020s

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://www.github.com/Yuppiechef/datomic-schema"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]]
   :profiles
   {:dev
    {:dependencies
-    [[com.datomic/datomic-free "0.9.5153"]]}})
+    [[com.datomic/peer "1.0.7277"]
+     [org.slf4j/slf4j-api "2.0.16"]
+     [org.slf4j/slf4j-nop "2.0.16"]]}})
